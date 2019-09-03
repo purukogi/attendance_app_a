f8e0a4c8a3cade2770425cac8110859eb5407736
Daisaku <d_sawada@hotmail.co.jp>
2019-09-03 15:17:44 +0000

    2019/09/04_save

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190901103454) do
+ActiveRecord::Schema.define(version: 20190903145432) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190901103454) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
