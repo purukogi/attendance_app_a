806259bd6cc0301b209fe8f55f9314651dfc6b32
Daisaku <d_sawada@hotmail.co.jp>
2019-11-22 14:56:27 +0000

    2019/11/22_save1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190929084118) do
+ActiveRecord::Schema.define(version: 20191122144529) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190929084118) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "work_description"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
