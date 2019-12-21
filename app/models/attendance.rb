cf7893ff50618657ddabcd5b72eab6587b7400b6
Daisaku <d_sawada@hotmail.co.jp>
2019-12-21 23:52:17 +0000

    2019/12/22_save1

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,10 @@
 class Attendance < ApplicationRecord
   require 'rounding'
   belongs_to :user
+  # 残業申請状態（0:無 1:申請中 2:承認 3:否認）
+  enum application_state: { nothing: 0, applying: 1, approval: 2, denial: 3 }
+  # 勤怠編集申請状態（0:無 1:申請中 2:承認 3:否認）
+  enum application_edit_state: { nothing0: 0, applying1: 1, approval2: 2, denial3: 3 }
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
