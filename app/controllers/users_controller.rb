3cd6c7ffed34b9c462a8cca6e1e315e74a61b1f8
Daisaku <d_sawada@hotmail.co.jp>
2019-12-26 23:30:22 +0000

    2019/12/27_save2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,9 @@ class UsersController < ApplicationController
       @worked_sum = @attendances.where.not(started_at: nil).count
     end
     
-    @applications_to_me = Attendance.where(application_state: :applying)
+    @applications_to_A = Attendance.where(authorizer_user_id: "上長Ａ", application_state: :applying)
+    @applications_to_B = Attendance.where(authorizer_user_id: "上長Ｂ", application_state: :applying)
+    
   end
   
   def ensure_correct_user
